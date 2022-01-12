061589c2eeb162fb0d75019d90bce2eaa70cfe70
AbrahamOsmondE <abraham.osmond@gmail.com>
2022-01-13 03:08:12 +0800

    Initialize Puzzle Model

--- backend/puzzles/models.py
+++ backend/puzzles/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from users.models import User
 # Create your models here.
+
+
+class Puzzle(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    letters = models.CharField(max_length=7, blank=False)
+    errors = models.IntegerField()
+    correct_answers = models.IntegerField()
+    possible_answers = models.IntegerField()
+
+    def __str__(self):
+        return self.letters
