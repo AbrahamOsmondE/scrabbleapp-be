1016d9fb8ec321fc1b5e36e8050d25c2d66c18fb
AbrahamOsmondE <abraham.osmond@gmail.com>
2022-02-07 05:27:45 +0800

    Update views.py

--- api/views.py
+++ api/views.py
@@ -8,34 +8,34 @@ from django.http import QueryDict
 import random
 import dill
 
-# with open("csw", "rb") as dill_file:
-#     csw = dill.load(dill_file)
+with open("csw", "rb") as dill_file:
+    csw = dill.load(dill_file)
 
-# with open("CSWTree", "rb") as dill_file:
-#     CSWTree = dill.load(dill_file)
+with open("CSWTree", "rb") as dill_file:
+    CSWTree = dill.load(dill_file)
 
-# # with open("dawg", "rb") as dill_file:
-# #     dawg = dill.load(dill_file)
+# with open("dawg", "rb") as dill_file:
+#     dawg = dill.load(dill_file)
 
-# with open("sevenLetterWords", "rb") as dill_file:
-#     seven_letter = dill.load(dill_file)
+with open("sevenLetterWords", "rb") as dill_file:
+    seven_letter = dill.load(dill_file)
 
 
 def getWords(request, word):
-    # dictionary = {}
-    # word = word.upper()
-    # permutations = [''.join(j) for i in range(1, len(word) + 1)
-    #                 for j in itertools.permutations(word, i) if CSWTree.is_word(''.join(j))]
-    # for combination in permutations:
-    #     if len(combination) in dictionary:
-    #         dictionary[len(combination)].add(combination)
-    #     else:
-    #         dictionary[len(combination)] = set([combination])
-    # for keys in dictionary:
-    #     dictionary[keys] = list(dictionary[keys])
-    # del permutations
-    # return JsonResponse(dictionary, safe=False)
-    # x = CSWTree.is_word(word.upper())
+    dictionary = {}
+    word = word.upper()
+    permutations = [''.join(j) for i in range(1, len(word) + 1)
+                    for j in itertools.permutations(word, i) if CSWTree.is_word(''.join(j))]
+    for combination in permutations:
+        if len(combination) in dictionary:
+            dictionary[len(combination)].add(combination)
+        else:
+            dictionary[len(combination)] = set([combination])
+    for keys in dictionary:
+        dictionary[keys] = list(dictionary[keys])
+    del permutations
+    return JsonResponse(dictionary, safe=False)
+    x = CSWTree.is_word(word.upper())
     return
 
 
@@ -129,96 +129,96 @@ def getWords(request, word):
 #     return JsonResponse(dictionary, safe=False)
 
 
-# def getDefinition(request, word):
-#     dictionary = {}
-#     word = word.upper()
-#     description = csw[word]
-#     if '[' in description:
-#         partOfSpeech = description[description.index(
-#             '[')+1:description.index(']')].split(" ")[0]
-#         definition = description.split('[')[0]
+def getDefinition(request, word):
+    dictionary = {}
+    word = word.upper()
+    description = csw[word]
+    if '[' in description:
+        partOfSpeech = description[description.index(
+            '[')+1:description.index(']')].split(" ")[0]
+        definition = description.split('[')[0]
 
-#         dictionary["definition"] = definition
-#         dictionary["partOfSpeech"] = partOfSpeech
-#         dictionary["points"] = countPoints(word)
-#     else:
-#         dictionary["definition"] = description
-#         dictionary["partOfSpeech"] = "n"
-#         dictionary["points"] = countPoints(word)
-#     return JsonResponse(dictionary, safe=False)
-
-
-# def solveBoard(request, rack):
-#     rack = rack.upper()
-#     rack = [i for i in rack]
-
-#     full_string = request.META['QUERY_STRING']
-#     full_string = full_string.strip()
-#     if full_string != '':
-#         board_entries = QueryDict(full_string)
-#         board = Board(15)
-#         for i in board_entries:
-#             row, col = i.split('-')
-#             board.set_tile((int(row), int(col)), board_entries.get(i))
-
-#         solver = SolveState(CSWTree, board, rack)
-
-#         solver.find_all_options()
-#         solver.answer.sort(reverse=True, key=lambda i: i['points'])
-
-#         answer = solver.answer[:min(len(solver.answer), 50)]
-
-#         del board
-#         del solver
-
-#         return JsonResponse(answer, safe=False)
-
-#     else:
-#         words = set()
-#         permutations = [''.join(j) for i in range(1, len(rack) + 1)
-#                         for j in itertools.permutations(rack, i) if CSWTree.is_word(''.join(j))]
-#         for combination in permutations:
-#             words.add(combination)
-#         words = list(words)
-
-#         board = Board(15)
-#         solver = SolveState(CSWTree, board, rack)
-#         solver.find_all_options_empty(words)
-#         solver.answer.sort(reverse=True, key=lambda i: i['points'])
-
-#         answer = solver.answer[:min(len(solver.answer), 50)]
-#         del board
-#         del solver
-#         del permutations
-#         del words
-#         return JsonResponse(answer, safe=False)
+        dictionary["definition"] = definition
+        dictionary["partOfSpeech"] = partOfSpeech
+        dictionary["points"] = countPoints(word)
+    else:
+        dictionary["definition"] = description
+        dictionary["partOfSpeech"] = "n"
+        dictionary["points"] = countPoints(word)
+    return JsonResponse(dictionary, safe=False)
 
 
-# def getPuzzle(request):
-#     puzzle = {}
-#     dictionary = {}
-#     letters = "".join(sorted(random.choice(seven_letter)))
+def solveBoard(request, rack):
+    rack = rack.upper()
+    rack = [i for i in rack]
 
-#     # letters = ["A"]*9 + ["B"]*2 + ["C"]*2 + ["D"]*4 + ["E"]*12 + ["F"]*2 + ["G"]*3 + ["H"]*2 + ["I"]*9 + ["J"]+["K"] + \
-#     #     ["L"]*4 + ["M"]*2+["N"]*6+["O"]*8+["P"]*2+["Q"]+["R"]*6 + \
-#     #     ["S"]*4+["T"]*6+["U"]*4+["V"]*2+["W"]*2+["X"]+["Y"]*2+["Z"]
-#     seven_letter_word = "".join(random.sample(letters, 7))
-#     permutations = [''.join(j) for i in range(1, 8) for j in itertools.permutations(
-#         seven_letter_word, i) if CSWTree.is_word(''.join(j))]
-#     for combination in permutations:
-#         if len(combination) in dictionary:
-#             dictionary[len(combination)].add(combination)
-#         else:
-#             dictionary[len(combination)] = set([combination])
-#     del permutations
-#     for keys in dictionary:
-#         dictionary[keys] = list(dictionary[keys])
-#     puzzle["count"] = 0
-#     for length in dictionary:
-#         dictionary[length].sort()
-#         puzzle["count"] += len(dictionary[length])
+    full_string = request.META['QUERY_STRING']
+    full_string = full_string.strip()
+    if full_string != '':
+        board_entries = QueryDict(full_string)
+        board = Board(15)
+        for i in board_entries:
+            row, col = i.split('-')
+            board.set_tile((int(row), int(col)), board_entries.get(i))
 
-#     puzzle["word"] = seven_letter_word
-#     puzzle["solutions"] = dictionary
-#     del dictionary
-#     return JsonResponse(puzzle, safe=False)
+        solver = SolveState(CSWTree, board, rack)
+
+        solver.find_all_options()
+        solver.answer.sort(reverse=True, key=lambda i: i['points'])
+
+        answer = solver.answer[:min(len(solver.answer), 50)]
+
+        del board
+        del solver
+
+        return JsonResponse(answer, safe=False)
+
+    else:
+        words = set()
+        permutations = [''.join(j) for i in range(1, len(rack) + 1)
+                        for j in itertools.permutations(rack, i) if CSWTree.is_word(''.join(j))]
+        for combination in permutations:
+            words.add(combination)
+        words = list(words)
+
+        board = Board(15)
+        solver = SolveState(CSWTree, board, rack)
+        solver.find_all_options_empty(words)
+        solver.answer.sort(reverse=True, key=lambda i: i['points'])
+
+        answer = solver.answer[:min(len(solver.answer), 50)]
+        del board
+        del solver
+        del permutations
+        del words
+        return JsonResponse(answer, safe=False)
+
+
+def getPuzzle(request):
+    puzzle = {}
+    dictionary = {}
+    letters = "".join(sorted(random.choice(seven_letter)))
+
+    # letters = ["A"]*9 + ["B"]*2 + ["C"]*2 + ["D"]*4 + ["E"]*12 + ["F"]*2 + ["G"]*3 + ["H"]*2 + ["I"]*9 + ["J"]+["K"] + \
+    #     ["L"]*4 + ["M"]*2+["N"]*6+["O"]*8+["P"]*2+["Q"]+["R"]*6 + \
+    #     ["S"]*4+["T"]*6+["U"]*4+["V"]*2+["W"]*2+["X"]+["Y"]*2+["Z"]
+    seven_letter_word = "".join(random.sample(letters, 7))
+    permutations = [''.join(j) for i in range(1, 8) for j in itertools.permutations(
+        seven_letter_word, i) if CSWTree.is_word(''.join(j))]
+    for combination in permutations:
+        if len(combination) in dictionary:
+            dictionary[len(combination)].add(combination)
+        else:
+            dictionary[len(combination)] = set([combination])
+    del permutations
+    for keys in dictionary:
+        dictionary[keys] = list(dictionary[keys])
+    puzzle["count"] = 0
+    for length in dictionary:
+        dictionary[length].sort()
+        puzzle["count"] += len(dictionary[length])
+
+    puzzle["word"] = seven_letter_word
+    puzzle["solutions"] = dictionary
+    del dictionary
+    return JsonResponse(puzzle, safe=False)
