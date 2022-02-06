e29bf3a703c65cc4bc4aac1bf11e9f40ebcd8aef
AbrahamOsmondE <abraham.osmond@gmail.com>
2022-02-07 03:35:20 +0800

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('wordbuilder/<str:word>', views.getWords, name='wordbuilder'),
+    # path('wordbuilder/<str:word>', views.getWords, name='wordbuilder'),
     # path('anagram/<str:word>', views.getAnagram, name='anagram'),
     # path('startingwith/<str:word>', views.getStartingWith, name='startingwith'),
     # path('endingwith/<str:word>', views.getEndingWith, name='endingwith'),
     # path('containing/<str:word>', views.getContaining, name='containingwith'),
-    path('definition/<str:word>', views.getDefinition, name='containingwith'),
-    path('solve/<str:rack>/', views.solveBoard, name='solve'),
-    path('puzzle', views.getPuzzle, name="puzzle")
+    # path('definition/<str:word>', views.getDefinition, name='containingwith'),
+    # path('solve/<str:rack>/', views.solveBoard, name='solve'),
+    # path('puzzle', views.getPuzzle, name="puzzle")
 ]
