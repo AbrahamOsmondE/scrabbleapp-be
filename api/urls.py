6796dc6668b651326a60c7e4140ebe346d44c88e
AbrahamOsmondE <abraham.osmond@gmail.com>
2022-02-07 04:00:21 +0800

    removed DAWG for memory

--- api/urls.py
+++ api/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path('wordbuilder/<str:word>', views.getWords, name='wordbuilder'),
-    path('anagram/<str:word>', views.getAnagram, name='anagram'),
-    path('startingwith/<str:word>', views.getStartingWith, name='startingwith'),
-    path('endingwith/<str:word>', views.getEndingWith, name='endingwith'),
-    path('containing/<str:word>', views.getContaining, name='containingwith'),
+    # path('anagram/<str:word>', views.getAnagram, name='anagram'),
+    # path('startingwith/<str:word>', views.getStartingWith, name='startingwith'),
+    # path('endingwith/<str:word>', views.getEndingWith, name='endingwith'),
+    # path('containing/<str:word>', views.getContaining, name='containingwith'),
     path('definition/<str:word>', views.getDefinition, name='containingwith'),
     path('solve/<str:rack>/', views.solveBoard, name='solve'),
     path('puzzle', views.getPuzzle, name="puzzle")
